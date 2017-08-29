7141a4f0587a4f1eb329b14dd8004c1987e8143d
Wei Zhu <yesmeck@gmail.com>
2017-08-29 18:00:54 +0800

    Add ant-design

--- src/generateCss.js
+++ src/generateCss.js
@@ -5,7 +5,6 @@ const postcss = require('postcss');
 const less = require('postcss-less-engine');
 const autoprefixer = require('autoprefixer');
 const rucksack = require('rucksack-css');
-const { promisify: py } = require('util');
 
 const PLACEHOLDER = '#999999';
 
@@ -45,19 +44,19 @@ const reducePlugin= postcss.plugin('reducePlugin', () => {
 });
 
 async function generateCss() {
-  const entry = '/Users/meck/Workspace/ant-design/components/style/index.less';
-  let css = (await py(fs.readFile)(entry)).toString();
-  const styles = await py(glob)('/Users/meck/Workspace/ant-design/components/*/style/index.less');
+  const antd = path.resolve(__dirname, '../ant-design');
+  const entry = path.join(antd, 'components/style/index.less');
+  let css = fs.readFileSync(entry).toString();
+  const styles = glob.sync(path.join(antd, 'components/*/style/index.less'));
   css += '\n';
   css += `@primary-color: ${PLACEHOLDER};\n`;
   css += '\n';
   for (let i = 0; i < styles.length; ++i) {
     css += `@import "${styles[i]}";\n`;
   }
-  fs.writeFileSync('/tmp/style.less', css);
   const output = await postcss([
     less({
-      paths: ['/Users/meck/Workspace/ant-design/components/style']
+      paths: [path.join(antd, 'components/style')]
     }),
     rucksack(),
     autoprefixer({
